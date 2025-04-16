9780c2d61d90d5e83a224574bf69bbafc11dfb6b
Your Name <your-email@example.com>
2025-04-16 23:43:22 +0530

    fixed node version and SSL issue

--- Server/db/dbconnect.js
+++ Server/db/dbconnect.js
@@ -1,19 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectMongo=()=>{
+const connectMongo = () => {
+  try {
+    const connect = mongoose.connect(`${process.env.MONGO_CONNECT_URL}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+    });
 
-    try {
-        const connect=mongoose.connect(`${process.env.MONGO_CONNECT_URL}`)
-        if(!connect){
-            console.log('Error while connecting to database');
-        }
-        else{
-            console.log('Successfully Connected To Database');
-        }
-    } catch (error) {
-        console.log(error);
+    if (!connect) {
+      console.log("Error while connecting to database");
+    } else {
+      console.log("Successfully Connected To Database");
     }
-
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default connectMongo;
